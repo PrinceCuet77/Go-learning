d6e488ec4bfc7a26de5f5bf0e59ab872e72aa209
Rezoan Shakil Prince <prince.cuet.77@gmail.com>
2022-12-08 11:33:03 +0600

    Side signing...

--- variable_basics/Types_and_Zero_values/main.go
+++ variable_basics/Types_and_Zero_values/main.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
+// Use mixedCase a.k.a camelCase instread of smake_case (variables or functions)
+var maxValue = 100
+
+func writeToFile() string {
+	return "Write to file"
+}
+
+// Write acronyms in all caps
+var writeToDB = true
+
+// An uppercase first letter has special significance to go (it will be exported in other packages)
+
 func main() {
-	// An uninitialized variable or empty variable will ge the so called Zero value.
+	// An uninitialized variable or empty variable will get the so called Zero value.
 	// The zero-value mechanism of go ensures that a variable always holds a well defined value of its type
 	var value int     // 0
 	var price float64 // 0
@@ -17,6 +29,8 @@ func main() {
 	/*
 		Multiple line comment
 	*/
+	var nameOfPerson = "John Wick" // Inline comment
+	_ = nameOfPerson
 
 	// Type casting
 	var fl float64 = 3.4
